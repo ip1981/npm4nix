d013913ab48a8ee664e84d74daf52211b7c9411d
Igor Pashev <pashev.igor@gmail.com>
2019-10-02 10:43:46 +0200

    Initial version 0.1.0

---
+++ README.md
@@ -0,0 +1,85 @@
+Npm4nix
+=======
+
+Npm4nix converts Node.js packages into a single Nix build expression
+for the [Npm.nix](https://github.com/ip1981/npm.nix) project. Npm4nix is
+inspired by [Cabal2nix](https://github.com/NixOS/cabal2nix). Unfortunately,
+[npm2nix](https://github.com/NixOS/npm2nix) is already taken :) Thus,
+`npm4nix`.  It supports local directories and tarballs (e. g. `*.tgz`),
+remote tarballs, remote [Git](https://git-scm.com/) respositories.
+
+
+Requirements
+============
+
+Npm4nix is written in JavaScript with minimum number of depednecies
+and should be executed by [Node.js](https://nodejs.org).
+Npm4nix invokes [curl](https://curl.haxx.se/), Git, and
+[nix-hash](https://nixos.org/nix/manual/#sec-nix-hash), so these tools
+should be installed.
+
+
+Example
+=======
+
+
+```
+$ node cmd/main.js https://github.com/substack/node-mkdirp.git > mkdirp.nix
+executing git clone 'https://github.com/substack/node-mkdirp.git' '/tmp/npm4nix-Feo4WM'
+Cloning into '/tmp/npm4nix-Feo4WM'...
+executing git -C '/tmp/npm4nix-Feo4WM' rev-parse HEAD
+executing nix-hash --base32 --type sha256 '/tmp/npm4nix-Feo4WM'
+
+$ cat mkdirp.nix
+{ fetchgit, buildNpmPackage, minimist, mock-fs, tap }:
+
+buildNpmPackage {
+  pname = "mkdirp";
+  version = "0.5.1";
+  src = fetchgit {
+    url = "https://github.com/substack/node-mkdirp.git";
+    rev = "f2003bbcffa80f8c9744579fabab1212fc84545a";
+    sha256 = "0qc3l6571aknhlmzcyaah3plmf852cl160jihy3l4b05j25qv45a";
+  };
+
+  meta = {
+    description = "Recursively mkdir, like `mkdir -p`";
+    homepage = "";
+    license = "MIT";
+  };
+
+  npmInputs = [
+    minimist mock-fs tap
+  ];
+}
+```
+
+```
+$ node cmd/main.js https://github.com/cowboy/javascript-sync-async-foreach/archive/v0.1.3.tar.gz > async-foreach.nix
+executing curl -LsSf -o '/tmp/npm4nix-zwGeOX/v0.1.3.tar.gz' 'https://github.com/cowboy/javascript-sync-async-foreach/archive/v0.1.3.tar.gz'
+executing nix-hash --flat --base32 --type sha256 '/tmp/npm4nix-zwGeOX/v0.1.3.tar.gz'
+executing tar -xOf '/tmp/npm4nix-zwGeOX/v0.1.3.tar.gz' --wildcards '*/package.json'
+
+$ cat async-foreach.nix
+{ fetchurl, buildNpmPackage }:
+
+buildNpmPackage {
+  pname = "async-foreach";
+  version = "0.1.3";
+  src = fetchurl {
+    url = "https://github.com/cowboy/javascript-sync-async-foreach/archive/v0.1.3.tar.gz";
+    sha256 = "1b7h2fgj6rndkviyx1hl0mh72d60a2b2f1sl86ndk8vdvr6mxmj3";
+  };
+
+  meta = {
+    description = "An optionally-asynchronous forEach with an interesting interface.";
+    homepage = "http://github.com/cowboy/javascript-sync-async-foreach";
+    license = "";
+  };
+
+  npmInputs = [
+
+  ];
+}
+
+```
---
+++ cmd/main.js
@@ -0,0 +1,173 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+const process = require('process');
+const {
+    execSync
+} = require('child_process');
+
+const render = require('../lib/template');
+
+var url = null,
+    revision = null;
+
+function log(msg) {
+    console.error(msg);
+}
+
+function printHelp() {
+    console.log(`
+npm4nix converts NPM packages into build instructions for Nix.
+
+Usage: npm4nix [options] URL
+
+Options:
+  -r, --revision revision to fetching from Git (tag, branch, hash)
+  -h, --help     show this help screen
+
+URL can be:
+  - a local directory with package.json
+  - a local archive (tarball)
+  - a remote Git URL
+  - a remote archive (tarball)
+
+Examples:
+
+  $ npm4nix -r 0.5.1 https://github.com/substack/node-mkdirp.git > mkdirp.nix
+  $ npm4nix . > mypkg.nix
+  `);
+}
+
+function exec(cmd) {
+    log(`executing ${cmd}`);
+    return execSync(cmd, {
+        encoding: 'utf-8'
+    }).trim();
+}
+
+function getSHA256(p) {
+    var flat = '';
+    if (fs.statSync(p).isFile()) {
+        flat = ' --flat';
+    }
+
+    return exec(`nix-hash${flat} --base32 --type sha256 '${p}'`);
+}
+
+function mkdtemp() {
+    const tmpdir = os.tmpdir();
+    return fs.mkdtempSync(`${tmpdir}${path.sep}npm4nix-`);
+}
+
+function rmTree(dir) {
+    fs.readdirSync(dir).forEach(function(entry, index) {
+        const p = path.join(dir, entry);
+        if (fs.lstatSync(p).isDirectory()) {
+            rmTree(p);
+        } else {
+            fs.unlinkSync(p);
+        }
+    });
+    fs.rmdirSync(dir);
+};
+
+function readPackage(p) {
+    var pkgFile = path.join(p, 'package.json');
+    return JSON.parse(fs.readFileSync(pkgFile));
+}
+
+function isLocal(p) {
+    return fs.existsSync(p);
+}
+
+function processLocal(p) {
+    console.log(render(p, readPackage(p)));
+}
+
+function isArchive(url) {
+    return url.match(new RegExp('(https?://)?.+/[^/]+\.t(ar\.)?(gz|bz2|xz)', 'i'));
+}
+
+function processArchive(url) {
+    const dir = mkdtemp();
+    const file = path.join(dir, path.basename(url));
+    var sha256, pkg;
+    try {
+        exec(`curl -LsSf -o '${file}' '${url}'`);
+        sha256 = getSHA256(file);
+        pkg = JSON.parse(exec(`tar -xOf '${file}' --wildcards '*/package.json'`));
+    } finally {
+        rmTree(dir);
+    }
+    console.log(render({
+        fetch: 'fetchurl',
+        url: url,
+        sha256: sha256
+    }, pkg));
+}
+
+function isGit(url) {
+    return url.match(new RegExp('(.*https://)?(github|gitlab)\.com/[^/]+/[^/]+(\.git)?$', 'i')) ||
+        url.match(new RegExp('(.*https://)?bitbucket\.org/[^/]+/[^/]+(\.git)?$', 'i')) ||
+        url.match(new RegExp('^git(\\+https)?://.+', 'i'));
+}
+
+function processGit(url) {
+    const dir = mkdtemp();
+    var rev, sha256, pkg;
+    try {
+        exec(`git clone '${url}' '${dir}'`);
+        if (revision !== null) {
+            exec(`git -C '${dir}' checkout '${revision}'`);
+        }
+        rev = exec(`git -C '${dir}' rev-parse HEAD`);
+        rmTree(path.join(dir, '.git'));
+        sha256 = getSHA256(dir);
+        pkg = readPackage(dir);
+    } finally {
+        rmTree(dir);
+    }
+    console.log(render({
+        fetch: 'fetchgit',
+        url: url,
+        rev: rev,
+        sha256: sha256
+    }, pkg));
+}
+
+
+const args = process.argv.slice(2);
+
+for (var i = 0; i < args.length; i++) {
+    switch (args[i]) {
+        case '-r':
+        case '--revision':
+            revision = args[++i];
+            break;
+        case '-h':
+        case '--help':
+            printHelp();
+            process.exit();
+            break;
+        default:
+            url = args[i];
+    }
+}
+
+if (url === null) {
+    log('Missing URL. Add --help for usage info');
+    process.exit(1);
+}
+
+if (isLocal(url)) {
+    processLocal(url);
+} else if (isGit(url)) {
+    processGit(url);
+} else if (isArchive(url)) {
+    processArchive(url);
+} else {
+    log(`unsupported URL: '${url}'`);
+    process.exit(1);
+}
